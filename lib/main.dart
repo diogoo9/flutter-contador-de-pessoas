d59e7ebc746901ef0255c194a9521bf7e787234e
Diogo Cipriano <di-ogo_51@hotmail.com>
2019-08-22 01:05:33 -0300

    app contador concluido

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,97 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    title: "Contador de pessoas",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+      title: "Contador de pessoas",
+      home: home()
+  )); //m
+}
+
+class home extends StatefulWidget {
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  int _people = 0;
+  String _infoText = "pode entrar!";
+
+  void _changePeople(int delta){
+    setState(() {
+      _people += delta;
+
+      if(_people < 0){
+        _infoText = "Mundo invertido!";
+      }else if(_people < 10){
+        _infoText = "Pode entrar!";
+      }else{
+        _infoText = "Lotado !!!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       children: <Widget>[
-        Text("Pessoas: 01",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        Image.asset(
+          "images/bg.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
         ),
-        Text("pode entrar",
-         style: TextStyle(color: Colors.white,
-                          fontStyle: FontStyle.italic,
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: $_people",
+              style:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
                           fontSize: 30.0),
+                    ),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0),
+                    ),
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              "$_infoText",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
         )
       ],
-    )
-  ));//m
+    );
+  }
 }
